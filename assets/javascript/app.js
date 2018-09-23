9795105d76e3c19407bd7ec6e316e77b0e4ccb7c
jorgegs14 <jorgeeduardo_gs@hotmail.com>
2018-09-22 18:17:09 -0600

    started coding

---
+++ assets/javascript/app.js
@@ -0,0 +1,11 @@
+
+$('#start-button').on("click", function(event){
+    $("#start-button").hide();
+    setTimeout(1000 * 60)
+    var timer = $("<div>")
+    console.log(setTimeout)
+})
+
+function countDown() {
+    
+}
